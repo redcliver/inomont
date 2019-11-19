fcd8e3d456f47036f0d14e018df629a3360fb2d9
igor.peres <igor-peres@hotmail.com>
2019-11-19 19:09:03 -0300

    Alteração na webpage
    alterações na tela principal do site

--- website/views.py
+++ website/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'site/home.html', {'title':'Home'})
+    colabRegistrados = 23244
+    colabTrabalhando = 12323
+    trabRealizado = 9856
+    return render(request, 'site/home.html', {'title':'Home',
+                                              'colabRegistrados':colabRegistrados,
+                                              'colabTrabalhando':colabTrabalhando,
+                                              'trabRealizado':trabRealizado})
